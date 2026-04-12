a844aff828cbfd1b1314f3f51aa8024b9ce263d0
Niruta Adhikari <adkniru184@gmail.com>
2026-04-13 00:57:45 +0545

    something chnages

--- server.js
+++ server.js
@@ -86,8 +86,7 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
-// Server
-// Start server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
    {
